cfc4925f34ca3ae02e81030bab23041fd32a8644
JP Montagnet <x-github@jope.net>
2022-04-23 21:51:24 -0700

    initial commit. working app.py. DOckerfile probably right.

---
+++ app.py
@@ -0,0 +1,22 @@
+# TCSS 506 Spring 2022 - homework 2
+# Student: JP Montagnet (jpmont)
+
+from flask import Flask
+from time import ctime
+
+app = Flask(__name__)
+
+@app.route("/")
+def f_root():
+    return "TCSS 506 - 2022 Springt - jpmont\n"
+
+@app.route("/owner")
+def f_owner():
+    return "Hello world from JP Montagnet (jpmont)\n"
+
+@app.route("/datetime")
+def f_datetime():
+    return f"{ctime()}\n"
+
+# END
+# vim: set ts=4 sw=4 expandtab :
